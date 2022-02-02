6925fcfeb8844946d2eeea307bfb895e74a29a83
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-02 10:16:01 -0800

    add text animate hook

---
+++ src/assets/hooks/textAnimate.js
@@ -0,0 +1,45 @@
+import { useState } from 'react'
+import styled from 'styled-components';
+
+const TextAnimateLi = styled.li`
+  color: black;
+  transition: color 0.2s;
+  cursor: pointer;
+  ${({ content, index }) => {
+    if (content[index].isActive === false) {
+      return `
+      color: rgba(0, 0, 0, 0.3);
+    `;
+    }
+  }}
+`;
+
+const useTextAnimate = (data) => {
+  const [content, setContent] = useState(data);
+
+  const setOneActive = (name) => {
+    let contentCopy = [...content];
+
+    contentCopy.forEach((item, index, arr) => {
+      if (arr[index].name !== name) {
+        arr[index].isActive = false;
+      }
+    });
+
+    setContent(contentCopy);
+  };
+
+  const setAllActive = () => {
+    let contentCopy = [...content];
+
+    contentCopy.forEach((item, index, arr) => {
+      arr[index].isActive = true;
+    });
+
+    setContent(contentCopy);
+  };
+
+  return { setOneActive, setAllActive, content };
+};
+
+export { useTextAnimate, TextAnimateLi }
