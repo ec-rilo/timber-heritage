ce53cfe39e446e4eee1dae0156d3ea1a8d13d342
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-02 22:29:52 -0800

    import timber-background to background

--- src/components/Templates/ComplexBanner1/ComplexBanner.js
+++ src/components/Templates/ComplexBanner1/ComplexBanner.js
@@ -1,6 +1,7 @@
 import styled from 'styled-components';
 import StyledUpperContainer from './UpperContainer';
 import StyledLowerContainer from './LowerContainer';
+import bgImg from './background-timber.webp';
 
 const ComplexBanner1 = ({ className }) => {
   return (
@@ -15,7 +16,7 @@ const StyledComplexBanner1 = styled(ComplexBanner1)`
   display: flex;
   flex-direction: column;
   gap: 100px;
-  background-color: var(--clr-baltic-sea);
+  background: url('${bgImg}');
   padding: 100px 15px;
   max-width: 1300px;
   margin: auto;
