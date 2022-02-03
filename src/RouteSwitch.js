3731f1598e1f5354e7b809dcfd2b4a0cfc863512
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-02 22:17:54 -0800

    import aos

--- src/RouteSwitch.js
+++ src/RouteSwitch.js
@@ -1,7 +1,11 @@
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
+import AOS from 'aos'
+import 'aos/dist/aos.css';
 
 const RouteSwitch = () => {
+  AOS.init();
+
   return (
     <BrowserRouter>
       <Routes>
