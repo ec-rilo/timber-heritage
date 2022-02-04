391a2340d82c80e68b62dfeb683b9deb685bdb4f
Edgar Carrillo <ecarrillo046@gmail.com>
2022-02-03 18:28:05 -0800

    add styledbtn

--- src/components/Templates/Buttons.js
+++ src/components/Templates/Buttons.js
@@ -1,5 +1,37 @@
-import styled from 'styled-components';
+import styled, { css } from 'styled-components';
 import device from '../../assets/devices';
+
+const Btn = ({ className, text }) => {
+  return (
+    <button className={className}>
+      {text}
+    </button>
+  );
+};
+
+const StyledBtn = styled(Btn)`
+  padding: 15px 50px;
+  background-color: var(--clr-dust);
+  border: none;
+  color: var(--clr-cultured);
+  font-size: 1rem;
+  border-radius: 5px;
+  transition: background-color 0.3s, color 0.3s;
+  cursor: pointer;
+
+  &:hover {
+    background-color: var(--clr-baltic-sea);
+  }
+
+  ${props =>
+    props.light &&
+    css`
+      &:hover {
+        background-color: var(--clr-cultured);
+        color: var(--clr-baltic-sea);
+      }
+    `};
+`;
 
 const Btn1 = ({ className, text }) => {
   return (
@@ -29,4 +61,4 @@ const StyledBtn1 = styled(Btn1)`
   }
 `;
 
-export { StyledBtn1 };
+export { StyledBtn1, StyledBtn };
